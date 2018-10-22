ced7d73cfee6a00d206d8eae2055cff9e4897507
Andrew Jye Shih Chuang <achuang@cpqd.com.br>
2018-10-22 15:55:16 -0300

    FaceOverlayFilter output only

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ var args = getopts(location.search,
   default:
   {
     ws_uri: 'ws://' + location.hostname + ':8888/kurento',
+    hat_uri: 'https://vignette.wikia.nocookie.net/liberproeliis/images/d/dc/Wing_mario_hat_copie_by_banjo2015-d8lf6e9.png/revision/latest?cb=20170407135810&path-prefix=pt-br',
     ice_servers: undefined
   }
 });
@@ -36,15 +37,19 @@ if (args.ice_servers) {
   console.log("Use ICE servers: " + args.ice_servers);
   kurentoUtils.WebRtcPeer.prototype.server.iceServers = JSON.parse(args.ice_servers);
 } else {
-  console.log("Use freeice")
+  // console.log("Use freeice")
 }
 
 
 window.addEventListener('load', function(){
   console = new Console('console', console);
-	var videoOutput = document.getElementById('videoOutput');
+
+  var videoInput = document.getElementById('videoInput');
+  var videoOutput = document.getElementById('videoOutput');
+
 	var address = document.getElementById('address');
-	address.value = 'http://files.kurento.org/video/puerta-del-sol.ts';
+  address.value = 'rtsp://admin:CPqd123$$@10.50.11.111/LiveMedia/ch1/Media1';
+  
   var pipeline;
   var webRtcPeer;
 
@@ -58,22 +63,28 @@ window.addEventListener('load', function(){
   	if(!address.value){
   	  window.alert("You must set the video source URL first");
   	  return;
-  	}
+    }
+    
   	address.disabled = true;
-  	showSpinner(videoOutput);
+  	showSpinner(videoInput);
+    
     var options = {
-      remoteVideo : videoOutput
+      localVideo : videoInput,
+      remoteVideo: videoOutput
     };
+    
     webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
       function(error){
         if(error){
           return console.error(error);
         }
+
         webRtcPeer.generateOffer(onOffer);
+
         webRtcPeer.peerConnection.addEventListener('iceconnectionstatechange', function(event){
           if(webRtcPeer && webRtcPeer.peerConnection){
-            console.log("oniceconnectionstatechange -> " + webRtcPeer.peerConnection.iceConnectionState);
-            console.log('icegatheringstate -> ' + webRtcPeer.peerConnection.iceGatheringState);
+            // console.log("oniceconnectionstatechange -> " + webRtcPeer.peerConnection.iceConnectionState);
+            // console.log('icegatheringstate -> ' + webRtcPeer.peerConnection.iceGatheringState);
           }
         });
     });
@@ -85,39 +96,56 @@ window.addEventListener('load', function(){
   	kurentoClient(args.ws_uri, function(error, kurentoClient) {
   		if(error) return onError(error);
 
-  		kurentoClient.create("MediaPipeline", function(error, p) {
-  			if(error) return onError(error);
-
-  			pipeline = p;
-
+  		kurentoClient.create("MediaPipeline", function(error, pipeline) {
+        if(error) return onError(error);
+        
   			pipeline.create("PlayerEndpoint", {uri: address.value}, function(error, player){
   			  if(error) return onError(error);
 
   			  pipeline.create("WebRtcEndpoint", function(error, webRtcEndpoint){
-  				if(error) return onError(error);
+            if(error) return onError(error);
 
-          setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError);
+            setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError);
 
-  				webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
-  					if(error) return onError(error);
+            webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
+              if(error) return onError(error);
 
-            webRtcEndpoint.gatherCandidates(onError);
+              webRtcEndpoint.gatherCandidates(onError);
 
-  					webRtcPeer.processAnswer(sdpAnswer);
-  				});
+              webRtcPeer.processAnswer(sdpAnswer);
+            });
 
-  				player.connect(webRtcEndpoint, function(error){
-  					if(error) return onError(error);
 
-  					console.log("PlayerEndpoint-->WebRtcEndpoint connection established");
+            pipeline.create('FaceOverlayFilter', function(error, filter){
+              if(error) return onError(error);
 
-  					player.play(function(error){
-  					  if(error) return onError(error);
+              console.log('Got FaceOverlayFilter');
 
-  					  console.log("Player playing ...");
-  					});
-  				});
-  			});
+              filter.setOverlayedImage(args.hat_uri, -0.35, -1.2, 1.6, 1.6, function(error){
+                if(error) return onError(error);
+
+                console.log('Set overlay image');
+              });
+
+              player.connect(filter, function(error){
+                if(error) return onError(error);
+                
+                console.log('player >>> filter');
+
+                filter.connect(webRtcEndpoint, function(error){
+                  if(error) return onError(error);
+
+                  console.log('filter >>> webRtcEndpoint');
+
+                  player.play(function(error){
+                    if(error) return onError(error);
+
+                    console.log('Player playing...');
+                  });
+                });
+              });
+            });
+  			  });
   			});
   		});
   	});
@@ -133,14 +161,14 @@ window.addEventListener('load', function(){
       pipeline.release();
       pipeline = null;
     }
-    hideSpinner(videoOutput);
+    hideSpinner(videoInput);
   }
 
 });
 
 function setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError){
   webRtcPeer.on('icecandidate', function(candidate){
-    console.log("Local icecandidate " + JSON.stringify(candidate));
+    // console.log("Local icecandidate " + JSON.stringify(candidate));
 
     candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
 
@@ -150,7 +178,7 @@ function setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError){
   webRtcEndpoint.on('OnIceCandidate', function(event){
     var candidate = event.candidate;
 
-    console.log("Remote icecandidate " + JSON.stringify(candidate));
+    // console.log("Remote icecandidate " + JSON.stringify(candidate));
 
     webRtcPeer.addIceCandidate(candidate, onError);
   });
